e9e2165c7f8a24cc388463ab2db188b29becdb2c
brad-h <brad.hawbaker@gmail.com>
2019-12-02 05:46:07 -0600

    fix FT for staging

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import WebDriverException
 import time
 import os
 from unittest import skip
+from .server_tools import reset_database
 
 MAX_WAIT = 10
 
@@ -24,9 +25,10 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        staging_server = os.environ.get("STAGING_SERVER")
-        if staging_server:
-            self.live_server_url = "http://" + staging_server
+        self.staging_server = os.environ.get("STAGING_SERVER")
+        if self.staging_server:
+            self.live_server_url = "http://" + self.staging_server
+            reset_database(self.staging_server)
     
     def tearDown(self):
         self.browser.quit()
